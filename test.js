19910acf2511be7a443597977e1494f3c753cea0
Khor Zhen Win <khorzhenwin@gmail.com>
2022-08-05 11:43:32 +0800

    some changes

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+module.exports = async () => {
+  const res = await request
+    .post("http://localhost:3000/api/v1/login")
+    .send(payload)
+    .set("Accept", "application/json")
+    .set("Content-Type", "application/json")
+    .set("Accept-Language", "en-US");
+};
