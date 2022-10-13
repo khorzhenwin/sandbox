9513c1eba7e8ba9be937c6b6cf7ac02c42930d08
Khor Zhen Win <khorzhenwin@gmail.com>
2022-10-13 11:32:24 +0800

    [Update] mothly check in

--- test.js
+++ test.js
@@ -1,6 +1,31 @@
 const subDoc = "contacts";
 const subDocId = "12345";
 const filter = { $pull: {} };
-filter.$pull[subDoc] = { id: subDocId };
+filter.$pull.subDoc = { id: subDocId };
 console.log(filter);
 console.log("");
+
+// binary search
+function binarySearch(arr, key) {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] < key) {
+      low = mid + 1;
+    } else if (arr[mid] > key) {
+      high = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
+
+() => {};
+
+const index = [
+  { code: "ABCD", name: "ABC" },
+  { code: "ABCD", name: "ABC" },
+  { code: "ABCD", name: "ABC" },
+];
