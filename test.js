2bd899842e08038e2ac8c8ff575739ab36ae612d
Khor Zhen Win <khorzhenwin@gmail.com>
2022-10-22 23:04:38 +0800

    testing timestamp in js

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
-const subDoc = "contacts";
-const subDocId = "12345";
-const filter = { $pull: {} };
-filter.$pull[subDoc] = { id: subDocId };
-console.log(filter);
-console.log("");
+// const subDoc = "contacts";
+// const subDocId = "12345";
+// const filter = { $pull: {} };
+// filter.$pull[subDoc] = { id: subDocId };
+// console.log(filter);
+// console.log("");
+
+const newDate1 = new Date("2022-7-1");
+console.log(newDate1);
+const newDate2 = new Date("2022-7-31");
+console.log(newDate2);
